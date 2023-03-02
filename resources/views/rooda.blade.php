03020905b0a14a5555ccac7b451a8d478a3612cc
Erfan Sabouri <erfaansabouri@gmail.com>
2023-03-02 22:12:31 +0330

    Fix

--- resources/views/rooda.blade.php
+++ resources/views/rooda.blade.php
@@ -2,16 +2,52 @@
 <html>
 <head>
     <title>Show Location Points on Image</title>
-    <style>
-        #myCanvas {
-            position: absolute;
-            top: 0;
-            left: 0;
-        }
-    </style>
+    <link rel="stylesheet" href="{{ asset('css/uikit.min.css') }}" />
+    <script src="{{ asset('js/uikit.min.js') }}"></script>
+    <script src="{{ asset('js/uikit-icons.min.js') }}"></script>
+    <script src="{{ asset('js/heatmap.min.js') }}"></script>
 </head>
+<style>
+    #myHeat {
+        background-image: url({{ asset('layouts/mari.png') }});
+        height: 500px;
+    }
+</style>
+
 <body>
-<canvas id="myCanvas"></canvas>
+<div uk-grid>
+    <div class="uk-card-media-left uk-cover-container">
+        <canvas id="myCanvas"></canvas>
+        <div id="myHeat"></div>
+    </div>
+    <div>
+        <div class="uk-card-body">
+            <h3 class="uk-card-title">Instructions</h3>
+            <form class="uk-form-horizontal uk-margin-large">
+                <div class="uk-margin">
+                    <label class="uk-form-label uk-text-bold" for="form-horizontal-text">Step 1</label>
+                    <div class="uk-form-controls">
+                        <span>Select origin point (0,0).</span> <span id="step-1" class="uk-text-bold uk-text-success"></span>
+                    </div>
+                </div>
+                <hr class="uk-divider-small">
+                <div class="uk-margin">
+                    <label class="uk-form-label uk-text-bold" for="form-horizontal-text">Step 2</label>
+                    <div class="uk-form-controls">
+                        <span>Select initial point ({{ $movements->first()->x }},{{ $movements->first()->y }}).</span> <span id="step-2" class="uk-text-bold uk-text-success"></span>
+                    </div>
+                </div>
+                <hr class="uk-divider-small">
+                <div class="uk-margin">
+                    <label class="uk-form-label uk-text-bold" for="form-horizontal-text">Draw movements pattern</label>
+                    <div class="uk-form-controls">
+                        <button type="button" class="uk-button uk-button-primary" disabled id="draw-movements-pattern">Draw</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
 <script>
     var distance_in_meter = "{{ $distance }}";
     var points = [];
@@ -37,11 +73,18 @@
         console.log(rectX, rectY)
         if (originPoint === null) {
             originPoint = {x: rectX, y: rectY};
+            var step_1 = document.getElementById('step-1');
+            step_1.textContent = `=> Done! (${rectX},${rectY})`;
             console.log("Origin done")
             drawPoint(originPoint);
         } else if (initialPoint === null) {
             initialPoint = {x: rectX, y: rectY};
             console.log("Initial Done")
+            var step_2 = document.getElementById('step-2');
+            step_2.textContent = `=> Done! (${rectX},${rectY})`;
+
+            var draw_movements_pattern_btn = document.getElementById('draw-movements-pattern');
+            draw_movements_pattern_btn.disabled = false;
             drawPoint(initialPoint);
             const distance_in_pixel = Math.sqrt(Math.pow(initialPoint.x - originPoint.x, 2) + Math.pow(initialPoint.y - originPoint.y, 2));
             console.log(`Distance in pixel: ${distance_in_pixel}px`);
@@ -49,19 +92,38 @@
             var scale = distance_in_meter / distance_in_pixel;
             console.log("Scale: " + scale);
         } else {
-            var pixel_points = [];
-            for (var i = 0; i < points.length; i++) {
-                pixel_points.push(mapPoint(points[i],{x: 0, y: 0}, originPoint,points[0], initialPoint));
-            }
 
-            ctx.moveTo(pixel_points[0].x, pixel_points[0].y);
-            for (var j = 1; j < pixel_points.length; j++) {
-                console.log(j + "-pixel mapped point", pixel_points[j])
-                console.log(j + "-meter mapped point", points[j])
-                drawPoint(pixel_points[j])
-            }
         }
     });
+
+    var draw_movements_pattern_btn = document.getElementById('draw-movements-pattern');
+    draw_movements_pattern_btn.addEventListener('click', function (event) {
+
+        if(originPoint === null || initialPoint === null) {
+            alert('err');
+        }else{
+            var pixel_points = [];
+            var heat_points = [];
+            for (var i = 0; i < points.length; i++) {
+                pixel_points.push(mapPoint(points[i],{x: 0, y: 0}, originPoint,points[0], initialPoint));
+                heat_points.push({x: pixel_points[i].x , y: pixel_points[i].y, value: 1});
+            }
+            for (var j = 0; j < pixel_points.length - 1; j++) {
+                drawLine(pixel_points[j], pixel_points[j+1])
+            }
+            var heatmapInstance = h337.create({
+                container: document.getElementById('myHeat')
+            });
+            heatmapInstance.setData({
+                max: 15,
+                data: heat_points
+            });
+            draw_movements_pattern_btn.classList.add('uk-button-success');
+            draw_movements_pattern_btn.classList.remove('uk-button-primary');
+            draw_movements_pattern_btn.textContent = 'Done! ' + pixel_points.length + " line drawn."
+        }
+    });
+
 
     // Function to draw a point on the canvas
     function drawPoint(point) {
@@ -69,6 +131,16 @@
         ctx.arc(point.x, point.y, 1, 0, 2 * Math.PI);
         ctx.fillStyle = 'red';
         ctx.fill();
+    }
+
+    function drawLine(pointA, pointB)
+    {
+        ctx.strokeStyle = 'blue';
+        ctx.lineWidth = 1;
+        ctx.beginPath();
+        ctx.moveTo(pointA.x, pointA.y);
+        ctx.lineTo(pointB.x, pointB.y);
+        ctx.stroke();
     }
 
     function mapPoint(pointToBeMapped, originPoint, originPointPrim, initialPoint, initialPointPrim) {
@@ -96,5 +168,6 @@
 
 
 </script>
+
 </body>
 </html>
